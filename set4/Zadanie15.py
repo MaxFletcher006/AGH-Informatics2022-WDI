4e20dd49191f0d595e1931f9f9214ef1cd153959
MaxFletcher006 <Bayarjavkhlan.b75@gmail.com>
2022-11-06 17:24:58 +0100

    update

--- set4/Zadanie15.py
+++ set4/Zadanie15.py
@@ -1,22 +1,13 @@
-#Complex numbers calculator Version 1.0
-import cmath
+x1 = int(input("Please enter the x1: "))
+y1 = int(input("Please enter the y1: "))
 
-x1 = int(input("Please enter x1: "))
-y1 = int(input("Please enter y1: "))
+x2 = int(input("Please enter the x2: "))
+y2 = int(input("Please enter the y2: "))
 
-x2 = int(input("Please enter x2: "))
-y2 = int(input("Please enter y2: "))
-
-
-#x1,y1 = raw_input(int("Please enter first complex expression: ")).split()
-#x2,y2 = raw_input(int("Please enter second complex expression: ")).split()  
-
-z1 = complex(x1,y1)
-z2 = complex(x2,y2)
-  
-print("\n" + "First imaginary complex number: " + str(z1) + "\n") 
-print("Second imaginary complex number: " + str(z2) + "\n") 
-print("Sum of two imaginary complex number: " + str((z1)+(z2)) + "\n")
-print("Substraction of two imaginary complex number: " + str((z1)-(z2)) + "\n")
-print("Multiply of two imaginary complex number: " + str((z1)*(z2)) + "\n")
-print("Divition of two imaginary complex number: " + str((z1)/(z2)) + "\n")
+print ("Sum of z1 and z2: (%d , %d(i))" %((x1+x2) , (y1+y2)))
+print ("Sub of z1 and z2: (%d , %d(i))" %((x1-x2) , (y1-y2)))
+print ("Mult of z1 and z2: (%d , %d(i^2) , %d(i))" %((x1*x2) , (y1*y2) , (x1*y2 + x2*y1)))
+print ("Div of z1 and z2: (%d ,%d(i))" %( ((x1*x2)+(y1*y2)/((x2**2)+(y2**2))) , ((x2*y1)-(x1*y2)/((x2**2)+(y2**2))) ))
+ 
+    
+    
